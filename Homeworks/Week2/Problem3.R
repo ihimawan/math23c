cdf14d69ebd0f04f3cd1e104a0ac04323b8ad088
ihimawan <indri.himawan18@yahoo.com>
2020-02-11 18:52:27 -0500

    corrections

--- Homeworks/Week2/Problem3.R
+++ Homeworks/Week2/Problem3.R
@@ -6,7 +6,7 @@ NC <- read.csv("lib/NCBirths2004_.csv");
 # Testing difference between mean weight is significant
 MaleAvg <- mean(NC$Weight[which(NC$Gender == "Male")])
 FemaleAvg <- mean(NC$Weight[which(NC$Gender == "Female")])
-observed <- MaleAvg - FemaleAvg; observed # Males are 103.26 ounces heavier
+observed <- MaleAvg - FemaleAvg # Males are 103.26 ounces heavier
 
 # permutation test
 N <- 10000
@@ -21,7 +21,13 @@ for (i in 1:N){
 mean(diffs)
 hist(diffs, breaks = "FD")
 abline(v = observed, col = "red")
-pvalue <- (sum(diffs >= observed)+1)/(N+1); pvalue # small number
+pvalue <- (sum(diffs >= observed)+1)/(N+1); pvalue # around 0.00059994
+pvalue.2tailed <- pvalue*2; pvalue.2tailed # around 0.00119988
+# the two tailed p-value says there is only 0.00119988 chance that the
+# observed discrepancy is by chance. If we are using the significance
+# level on .05, we have enough evidence against the null hypothesis that there
+# is no difference in mean birth weights and toward the alternative hypothesis
+# that there is a difference. Shown by how Male babies are heavier.
 
 # Testing significance of tobacco with birth weight
 NoTobaccoAvg <- mean(NC$Weight[which(NC$Tobacco == "No")])
@@ -41,4 +47,11 @@ for (i in 1:N){
 mean(diffs)
 hist(diffs, breaks = "FD")
 abline(v = observed, col = "red")
-pvalue <- (sum(diffs >= observed)+1)/(N+1); pvalue # small number
+pvalue <- (sum(diffs >= observed)+1)/(N+1); pvalue # small number around 9.999e-05
+pvalue.2tailed <- pvalue * 2; pvalue.2tailed # small number around .00019998
+# the two tailed p-value says there is only 0.00019998 chance that the
+# observed discrepancy is by chance. If we are using the significance
+# level on .05, we have enough evidence against the null hypothesis that there
+# is no difference in mean birth weights and toward the alternative hypothesis
+# that there is a difference. Shown by how babies whose moms don't use tobacco
+# is heavier.
--- Homeworks/Week2/Problem4.R
+++ Homeworks/Week2/Problem4.R
@@ -10,26 +10,24 @@ Unit2 <- "BRIG"
 unitBattles <- subset(battle, Abbr == Unit1 | Abbr == Unit2)
 
 # Find the average of kills - lost
-unit1Battles <- subset(unitBattles, Abbr == Unit1)
-unit2Battles <- subset(unitBattles, Abbr == Unit2)
-scoreUnit1 <- unit1Battles$Kills - unit1Battles$Lost
-scoreUnit2 <- unit2Battles$Kills - unit2Battles$Lost
-observed <- mean(scoreUnit2) - mean(scoreUnit1) # 14.69173; it can be shown here that BRIG is winning
+score <- unitBattles$Kills - unitBattles$Lost
+idx <- which(unitBattles$Abbr == Unit2)
+observed <- mean(score[idx]) - mean(score[-idx]) # 14.69173; it can be shown here that BRIG is winning
 
 N <- 10000
 diffs <- numeric(N)
 for (i in 1:N){
   # get sample of rows from the sub table which only contains BRIG and MEZU
-  index <- sample(nrow(unitBattles), sum(unitBattles$Abbr == Unit1))
-  selectedBattles <- unitBattles[index,]
-
-  # calculate again the average of kills - lost
-  unit1Battles <- subset(selectedBattles, selectedBattles$Abbr == Unit1)
-  unit2Battles <- subset(selectedBattles, selectedBattles$Abbr == Unit2)
-  scoreUnit1 <- unit1Battles$Kills - unit1Battles$Lost
-  scoreUnit2 <- unit2Battles$Kills - unit2Battles$Lost
-  diffs[i] = mean(scoreUnit2) - mean(scoreUnit1)
+  idx <- sample(nrow(unitBattles), sum(unitBattles$Abbr == Unit2))
+  diffs[i] = mean(score[idx]) - mean(score[-idx])
 }
+mean(diffs) # -0.005671234
 hist(diffs, breaks = "FD")
 abline(v = observed, col = "red")
-# From this historgram, it can be shown that "BRIG" has a more consistent win than "MEZU"
+pvalue <- (sum(diffs >= observed)+1)/(N+1); pvalue # around 0.01569843
+pvalue.2tailed <- pvalue * 2; pvalue.2tailed # around .03139686
+# the two tailed p-value says there is only 0.03139686 chance that the
+# observed discrepancy is by chance. If we are using the significance
+# level on .05, we have enough evidence against the null hypothesis that there
+# is no difference in mean kills minus lost between two Abbrs and toward the
+# alternative hypothesis that there is a difference.
