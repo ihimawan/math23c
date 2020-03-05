b197a46a829a6e19d93dd23d75b8be204b50873d
ihimawan <indri.himawan18@yahoo.com>
2020-03-04 21:33:27 -0500

    homeworks

--- Homeworks/Week5/Problem1.R
+++ Homeworks/Week5/Problem1.R
@@ -2,6 +2,7 @@
 # cleanup
 rm(list = ls())
 
+# A) Divide into (2^N)^3 dyadic cubes
 f1 <- function (N) {
   delta <- 1/2^N # width of each dyadic cube
   left <- seq(from = 0, to = 1 - delta, by = delta)
@@ -34,3 +35,35 @@ var(Repeat[2,]) # close to zero, which says that our approximation is pretty goo
 # sample mean and variance of winning probability
 mean(Repeat[3,]) #0.4444365, close to 4/9 done in problem 5B
 var(Repeat[3,]) # close to zero, which says that our approximation is pretty good
+
+# B) using unif function
+f2 <- function(N) {
+  numCubes <- (2^N)^3
+  #Since we want numCubes of x, y, z coordinates
+  X <- runif(numCubes, 0, 1)
+  Y <- runif(numCubes, 0, 1)
+  Z <- runif(numCubes, 0, 1)
+
+  mean.mine <- mean(Z^2)
+  mean.opp <- mean(X*Y)
+  prob.win <- mean(Z^2 < X*Y)
+
+  return (c(mean.mine, mean.opp, prob.win))
+}
+
+# now do it 25 times. Strangely this is faster than the for loop
+Repeat <- replicate(n=25, f2(6)); head(Repeat)
+
+# finding sample mean and sample variance of my score
+mean(Repeat[1,]) #0.3332762, close to 1/3 done in problem 5A
+var(Repeat[1,]) # close to zero, which says that our approximation is pretty good
+
+# finding sample mean and sample variance of opponent score
+mean(Repeat[2,]) #0.2499726, close to 1/4 done in problem 5A
+var(Repeat[2,]) # close to zero, which says that our approximation is pretty good
+
+# sample mean and variance of winning probability
+mean(Repeat[3,]) #0.4445734, close to 4/9 done in problem 5B
+var(Repeat[3,]) # close to zero, which says that our approximation is pretty good
+
+# noticing that the monte carlo method is more precise because the variation are much smaller.
--- Homeworks/Week5/Problem3.R
+++ Homeworks/Week5/Problem3.R
@@ -3,23 +3,39 @@
 rm(list = ls())
 
 # A) generate 10^4 random numbers from Pareto distribution
-
-randFx <- function(x) 4*(x)^(-5)
-randParetoNbrs <- randFx(runif(10^4, min=1, max=5))
+library(EnvStats)
+# randFx <- function(x) 4*(x)^(-5)
+# we know that the location is 1 and shape 4 for this pareto function.
+# So using that instaed of this function
+randParetoNbrs <- rpareto(10^4,1,4)
 hist(randParetoNbrs, probability = TRUE, breaks = "FD")
-curve(4*x^(-5), from = 0, to=5, add=TRUE)
+curve(dpareto(x, 1, 4), add=TRUE, col = "red", lwd = 3)
+# matches well
 
 # B) is Pareto distribution a good model
-# TODO need buckets?
 
 # I was unable to install the package (computer constantly stops working when installing).
 # So I am using this as alternative:
 load(file = "lib/danishuni.rda");
 ds <- get("danishuni"); head(ds)
 
-expected <- dpois(0:2, lambda);Expected[4]<-162*(1-ppois(3, lambda))
-observed <- ds$Loss
-hist(ds$Loss, probability = TRUE, breaks = "FD")
-curve(4*x^(-5), from = 1, to=250, add=TRUE, color = "red")
+bins <- qpareto(0.1*(0:10), 1, 4)
+loss <- ds$Loss
+bincode <- cut(loss, breaks=bins)
+bincode <- ifelse (loss==1, 1, bincode); bincode
+
+# sanity check to make sure all data are counted for
+sum(table(loss)) # 2167
+length(loss) # 2167
 
 
+observed <- as.vector(table(bincode))
+expected <- rep(sum(observed)/10,10)
+chisq <- sum((observed-expected)^2/expected); chisq # 4113.706 which is big
+
+# 10 categories - 1 (because imposed actual and expected total) - 2 (imposed to parameters on data)
+# = 7 degrees of freedom
+
+pvalue <- pchisq(chisq, df = 7, lower.tail = FALSE); pvalue # 0
+# Since it is zero, there is zero chance that the the data is from a Pareto distribution.
+# We have enough evidence to say that this data is not of Pareto distribution.
