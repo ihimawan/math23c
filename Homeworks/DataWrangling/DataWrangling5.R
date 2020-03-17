909a5144c41bac4a67bfd5848f38e07caa266ec0
ihimawan <indri.himawan18@yahoo.com>
2020-03-16 20:56:32 -0400

    a

---
+++ Homeworks/DataWrangling/DataWrangling5.R
@@ -0,0 +1,12 @@
+# Data Wrangling Problem 5
+#
+# Collect on your own (do NOT take data from the Internet), some data for which it is a reasonable hypothesis that the
+# number of counts in variousbins ought to obey a Poisson distribution.  Examples:  emails per hour orper day, number of
+# students arriving for lunch at Annenberg per minute.Save the resut in a .csv file(which might have just one column.)
+# Display a histogram of your data side-by-side with a Poisson distribution that has the same mean.  and use a chi-square
+# test to assess the match.  The prototypeis the analysis of Phillies’ home runs in script 3
+#
+# For this task, I took the most annoying book in the world, "How to Win Friends and Influence People" and counted the
+# frequency of the letter 'e' (either lower case or upper case) per 1000 characters.
+#
+# Oh no, I wouldn't count this by hand.
