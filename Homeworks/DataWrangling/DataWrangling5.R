fd6ebf3bde58140b37b65dd455eec293be3e9c96
ihimawan <indri.himawan18@yahoo.com>
2020-03-16 23:01:01 -0400

    progress on DW5

--- Homeworks/DataWrangling/DataWrangling5.R
+++ Homeworks/DataWrangling/DataWrangling5.R
@@ -7,6 +7,57 @@
 # test to assess the match.  The prototypeis the analysis of Phillies’ home runs in script 3
 #
 # For this task, I took the most annoying book in the world, "How to Win Friends and Influence People" and counted the
-# frequency of the letter 'e' (either lower case or upper case) per 1000 characters.
+# frequency of the letter 'e' (either lower case or upper case) per 1000 alphabetic characters (i.e. I would not include
+# spaces, new lines, special characters, numbers, etc.).
 #
-# Oh no, I wouldn't count this by hand.
+# Oh no, I wouldn't count this by hand. I have the python code in the `helpers` directory if you'd like to take a look at
+# my logic.
+
+# setwd("Homeworks/DataWrangling")
+rm(list = ls())
+
+eFrequencies <- read.csv("lib/numberOfE.csv"); head(eFrequencies)
+#       Range Frequency
+# 1     0-999       129
+# 2 1000-1999       112
+# 3 2000-2999       109
+# 4 3000-3999       148
+# 5 4000-4999       132
+# 6 5000-5999       123
+
+frequency <- eFrequencies$Frequency;
+
+barplot(table(frequency))
+
+# if this is a poisson distribution, find the mean
+lambda <- mean(frequency); lambda # 125.9347
+
+observed <- table(frequency)
+# seeing many tiny counts. going to try to clump them into buckets of:
+# x <= 113, 116, 117, ..., 140 <= x
+
+observed[15] <- sum(observed[0:15])
+observed <- observed[-(0:14)]
+observed[28] <- sum(observed[28:length(observed)])
+observed <- observed[-(29: length(observed))]; observed
+# 113 114 115 116 117 118 119 120 121 122 123 124 125 126 127 128 129 130 131 132 133 134 135 136 137 138 139 140
+#  38   6   7  15  10   4  16  11  16  11   8   9   9  14  10  14  14  14  11  12   7  11   6  9   8  10   5  32
+#
+# Looking good!
+
+# c(dpois(0:13, lambda), ppois(13, lambda, lower.tail = FALSE))
+
+N <- nrow(eFrequencies)
+expected <- c(sum(dpois(0:112, lambda)), dpois(113:140, lambda), ppois(140, lambda, lower.tail = FALSE))
+sum(expected) # must sum up to 1
+
+barplot(expected)
+barplot(observed)
+
+ChiSq <-function(Obs,Exp){
+  sum((Obs-Exp)^2/Exp)
+}
+
+chisq <- sum((observed-expected)^2/expected); chisq
+pValue <- pchisq(chisq, df = 14, lower.tail = FALSE); pValue
+chisq.test(observed)
--- Homeworks/DataWrangling/helpers/countE.py
+++ Homeworks/DataWrangling/helpers/countE.py
@@ -1,19 +1,34 @@
-from tika import parser
+import csv
 
-# creating a pdf file object
-pdfFileObj = open('book.pdf', 'rb')
+# changeable constants
+interval = 1000
+fileName = "howToWinFriendsAndInfluencePeople.txt"
 
-# creating a pdf reader object
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+# results
+frequencies = []
 
-# printing number of pages in pdf file
-print(pdfReader.numPages)
+# read the book and count the number of e's
+with open(fileName) as f:
+  charCounter = 0
+  currentCount = 0
+  while True:
+    c = f.read(1)
+    if c:
+      if ('a' <= c <= 'z') or ('A' <= c <= 'Z'):
+        charCounter += 1
+        if c == 'e' or c == 'E':
+          currentCount += 1
+        if charCounter >= interval:
+          frequencies.append(["%d-%d" % (len(frequencies) * interval, (len(frequencies) + 1) * interval - 1),
+                              currentCount])  # store the count in this interval
+          charCounter = 0  # reset counter once reached interval
+          currentCount = 0  # then reset to zero as well
+    else:
+      print("File is done reading")
+      break
 
-# creating a page object
-pageObj = pdfReader.getPage(0)
-
-# extracting text from page
-print(pageObj.extractText())
-
-# closing the pdf file object
-pdfFileObj.close()
+# file is done reading, convert list to csv
+frequencies.insert(0, ['Range', 'Frequency'])
+with open('../lib/numberOfE.csv', 'w', newline='') as file:
+  writer = csv.writer(file)
+  writer.writerows(frequencies)
