6ce28969af7a09894cb286cdffb06110432f6d76
ihimawan <indri.himawan18@yahoo.com>
2020-03-23 21:19:31 -0400

    poop

---
+++ Homeworks/Week7/Problem1.R
@@ -0,0 +1,17 @@
+# These replace the R take-home test that was advertised in the syllabus.
+#
+# 1.  Go  to  the  Internet  and  find  one  large  or  two  small  datasets  related
+# to respiratory viral infectons, either Covid-19 or something historical like the
+# “Spanish  flu”  of  1918  or  the  “swine  flu”  of  2009.   They  just  have
+# to  besufficient for doing the problems that follow.  Convert the data to a .csv file
+# that can be used by other members of the class.
+#
+# 2.  Using your datasets, make a boxplot and a scatter plot that show interesting relationships.
+#
+# 3.  Formulate an interesting hypothesis and test it by usng a permutation test.
+#
+# 4.  Find some aspect of your datasets than can be modeled by a discrete dis-tribution,
+# and support your model with histograms and a chi-square test.
+#
+# 5.  Find some aspect of your datasets than can be modeled by a continuous distribution,
+# and support your model with a histogram overlaid with a curveplus a chi-square test.
