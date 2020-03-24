89441f46062c056faae4d11332136c626aa822f5
ihimawan <indri.himawan18@yahoo.com>
2020-03-23 21:00:45 -0400

    poop

---
+++ Homeworks/DataWrangling/DataWrangling6.R
@@ -0,0 +1,7 @@
+# Find  on  the  Internet  some  data  that  might  plausibly  have  been  sampled from  a
+# continuous  distribution  (exponential,  gamma,  chi  square,  beta).
+# Dispaly  a  histogram  of  your  data  and  overlay  on  it  a  graph  of  the
+# den-sity function for your hypothesized distribution.  Then use integration is R
+# to compute the expected number of counts in various bins,  and compare with the observed
+# number of counts.  The prototype is the analysis for the exponential distribution in script
+# 4D, but there the data were preassigned to bins.
---
+++ Homeworks/DataWrangling/DataWrangling7.R
@@ -0,0 +1,5 @@
+# Find on the Internet some data for which the histogram has a “bell curve”shape.
+# Display a histogram of your data and overlay on it a graph of thenormal density
+# function for the same mean and variance.  Compare withthe  normal  distribution
+# by  using  deciles.   Then  repeat,  using  the  meanfor samples of modest size.
+# The prototype is the analysisof babies’ birthweights in script 5D.
