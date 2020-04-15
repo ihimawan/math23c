eed208dfe5013ee24b8fc60b0f9c3583fc346caf
ihimawan <indri.himawan18@yahoo.com>
2020-04-15 19:00:53 -0400

    problem 1

--- Homeworks/Week9/Problem1.R
+++ Homeworks/Week9/Problem1.R
@@ -5,6 +5,9 @@ rm(list=ls())
 "Use  Gram-Schmidt  to  construct  a  “new”  orthonormal  basis  for V,  and graph the four new basis vectors on a
 single set of axes.  Feel free to reusecode from script 9M, topic 4"
 
+#install.packages("pracma")
+library(pracma)
+
 #We use Gram-Schmidt to create an orthonormal basis for polynomials
 #of degree 3 or less on the interval [0, Inf].
 #A polynomial is represented by a vector of four coefficients:
@@ -14,8 +17,13 @@ x2 <- c(0,0,1,0)
 x3 <- c(0,0,0,1)
 
 #evaluate a polynomial function, given its vector of coefficients.
-polyEval <- function (coeff,x) {
-  sum(coeff*c(exp(-x), x*exp(-x), x^2*exp(-x), x^3*exp(-x)))
+f0 <- function(x) exp(-x)
+f1 <- function(x) x * exp(-x)
+f2 <- function(x) x^2 * exp(-x)
+f3 <- function(x) x^3 * exp(-x)
+
+polyEval <- function (coeff, x) {
+  sum(coeff*c(f0(x), f1(x), f2(x), f3(x)))
 }
 
 # multiply the functions then integrate the product from 0 to Inf.
@@ -28,16 +36,59 @@ inner(x0,x0); inner(x1,x1); inner(x2,x2)   #looks correct
 
 #First do the even-degree polynomials.
 v0 <- x0/sqrt(inner(x0,x0)); v0   #vector of length 1
-#Use Gram-Schmidt to make an orthonormal basis.
-y2 <- x2 - inner(x2,v0)*v0;
-v2 <- y2/sqrt(inner(y2,y2)); v2   #vector of length 1
+sqrt(inner(v0, v0)) # 1 - means it is normalized
 
-#Now do the odd-degree polynomials.
-v1 <- x1/sqrt(inner(x1,x1)); v1   #vector of length 1
-#Use Gram-Schmidt to make an orthonormal basis vector
-y3 <- x3 - inner(x3,v1)*v1; y3
-v3 <- y3/sqrt(inner(y3,y3)); v3   #vector of length 1
+#find the next v1
+u1 <- x1 - inner(x1,v0)*v0; u1 # unnormalized
+v1 <- u1/sqrt(inner(u1,u1)); v1
+sqrt(inner(v1, v1)) # 1 - means it is normalized
 
-#Success: we now have 4 orthogonal polynomials, all of length 1
-inner(v0,v0); inner(v1,v1); inner(v2,v2); inner(v3,v3)    #length is 1
-inner(v0,v1); inner(v1,v3)   # TODO NOT DOIN IT #orthogonal since multiplication is zero (or very close to zero)
+# check if v0 and v1 are orthogonal
+inner(v0,v1) # basically zero - orthogonal!
+
+#find v2
+u2 <- x2 - inner(x2,v0)*v0 - inner(x2,v1)*v1; u2
+v2 <- u2/sqrt(inner(u2,u2)); v2   #vector of length 1
+sqrt(inner(v2, v2)) # 1 - means it is normalized
+
+# check if v2 are orthogonal to v0 and v1
+inner(v0,v2) # basically zero - orthogonal!
+inner(v1,v2) # basically zero - orthogonal!
+
+# find v3
+u3 <- x3 - inner(x3,v2)*v2 - inner(x3,v1)*v1 - inner(x3,v0)*v0; u3
+v3 <- u3/sqrt(inner(u3,u3)); v3   #vector of length 1
+sqrt(inner(v3, v3)) # 1 - means it is normalized
+
+# check if v3 are orthogonal to v0 and v1 and v2
+inner(v3,v0) # basically zero - orthogonal!
+inner(v3,v1) # basically zero - orthogonal!
+inner(v3,v2) # basically zero - orthogonal!
+
+# check all three are basis..
+# which means they're lineraly independent vectors
+# row reducie is a way to find out. but we have a package.
+A <- cbind(v0,v1,v2,v3); A
+rref(A)
+#      v0 v1 v2 v3
+# [1,]  1  0  0  0
+# [2,]  0  1  0  0
+# [3,]  0  0  1  0
+# [4,]  0  0  0  1
+
+# Since we get identity, they are lineraly independent. Hence, they are basis.
+
+"graph the four new basis vectors on a single set of axes"
+
+v0; v1; v2; v3 # these are concrete representation of ORTHONORMAL basis.
+
+#Now, write functions for ABSTRACT basis functions
+a0 <- function (x) { v0[1] * f0(x) + v0[2] * f1(x) + v0[3] * f2(x) + v0[4] * f3(x) }
+a1 <- function (x) { v1[1] * f0(x) + v1[2] * f1(x) + v1[3] * f2(x) + v1[4] * f3(x) }
+a2 <- function (x) { v2[1] * f0(x) + v2[2] * f1(x) + v2[3] * f2(x) + v2[4] * f3(x) }
+a3 <- function (x) { v3[1] * f0(x) + v3[2] * f1(x) + v3[3] * f2(x) + v3[4] * f3(x) }
+
+curve(a0, from=-1, to=1, col="red")
+curve(a1, from=-1, to=1, col="green", add = TRUE)
+curve(a2, from=-1, to=1, col="blue", add = TRUE)
+curve(a3, from=-1, to=1, col="pink", add = TRUE)
