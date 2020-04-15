ec5353debfa2b9faca4fa220678567ac91c1a4af
ihimawan <indri.himawan18@yahoo.com>
2020-04-15 19:49:19 -0400

    r problems done

--- Homeworks/Week9/Problem2.R
+++ Homeworks/Week9/Problem2.R
@@ -4,9 +4,36 @@ rm(list=ls())
 
 "The following vector shows the predicted temperature in Phoenix, AZ at
 3-hour intervals, from 5 AM on April 8, 2020 to 5 AM on April 9, 2020."
-temp <- c(59,58,71,80,77,67,62,58,55)
 
 "Using Lagrange interpolation, fit a polynomial of degree 8 to these data,
-and use it to estimate the temperature at 7 PM. Draw a graph of this
-polynomial, with the specified data points shown by pch = 24"
+and use it to estimate the temperature at 7 PM."
 
+temp <- c(59,58,71,80,77,67,62,58,55)
+interval <- 0:(length(temp)-1) # x-values for temp above
+
+# create P^{-1} matrix to find P
+Pinv <- cbind(1, interval, interval^2, interval^3,
+              interval^4, interval^5, interval^6,
+              interval^7, interval^8)
+Pinv # this is a 9x9 matrix
+P <- solve(Pinv)
+
+# find interp function
+interp <- function(x, val) {
+  # this function relative to old basis
+  c(1,x,x^2,x^3,x^4,x^5,x^6,x^7,x^8)%*%P%*%val
+}
+
+# estimate temp at 7PM
+eval <- (19-5+3)/3; #17/3
+interp(eval, temp) #  62.84198 as the estimated temperature for 7PM
+
+"Draw a graph of this polynomial, with the specified data points shown by pch = 24"
+
+# plot using sequence of 500 values evenly spaced between 0 to 8
+xval <- seq(0,8, length.out = 500)
+fval <- sapply(xval, interp, val=temp)
+
+# looks pretty good.
+plot (xval, fval, type="l", xlab = "hours after 5 in 3-hour increments", ylab="Temp")
+points(interval, temp, pch=24)
--- Homeworks/Week9/Problem3.R
+++ Homeworks/Week9/Problem3.R
@@ -9,7 +9,99 @@ nia has a rainy season and a dry season, and the annual cycle should be
 revealed by Fourier analysis. Analyze the precipitation data in the le
 LAWeather.csv using the same approach that was used for Boston temper-
 ature in workshop problem 2. Introduce a variable ncoeff for the number
-of Fourier coecients. Draw graphs in R to show the following:
- ncoe =10 discovers the annual cycle.
- ncoe = 100 reveals that some years are rainier than others.
- ncoe = 1000 does a very good reconstruction."
+of Fourier coecients."
+
+data <- read.csv("lib/LAWeather.csv")
+precip <- data$precip; head(precip)
+n <- length(precip)
+
+# let's try to plot this
+plot (1:n, precip, type="l")
+
+# cosine & sine function from lecture
+myCos <- function(m) cos((1:n)*m*2*pi/n)
+mySin <- function(m) sin((1:n)*m*2*pi/n)
+
+plot(1:n, precip, type="l")
+points(1:n, 100*myCos(1), type="l", col="magenta")
+points(1:n, 100*mySin(1), type="l", col="red")
+points(1:n, 100*myCos(10), type="l", col="green")
+
+# Evaluate the coefficients by Euler's formula 
+coeffA <- function(m) sum(precip*myCos(m)/n)
+coeffB <- function(m) sum(precip*mySin(m)/n)
+
+"ncoef=10 discovers the annual cycle."
+
+ncoeff <- 10
+
+FourierA <- sapply(1:ncoeff,coeffA)
+FourierB <- sapply(1:ncoeff,coeffB)
+Fourier <- sqrt(FourierA^2+FourierB^2)
+
+# first find the index that contains max value
+largestIndex <- which.max(Fourier)
+
+points(1:n, mean(precip) +
+  FourierA[largestIndex]*myCos(largestIndex) +
+  FourierB[largestIndex]*mySin(largestIndex), type = "l", col = "red",lwd = 2)
+
+# reconstruct
+recon <- mean(precip)
+for (m in 1:ncoeff) {
+  recon <- recon + FourierA[m]*myCos(m)+FourierB[m]*mySin(m)
+}
+
+plot(1:n, precip,type = "l")
+points(1:n,recon, type = "l", col = "red",lwd = 2)
+# looks like annual cycle (because there is pretty smooth and not much variation)
+
+"ncoef=100 reveals that some years are rainier than others."
+
+ncoeff <- 100
+
+FourierA <- sapply(1:ncoeff,coeffA)
+FourierB <- sapply(1:ncoeff,coeffB)
+Fourier <- sqrt(FourierA^2+FourierB^2)
+
+# first find the index that contains max value
+largestIndex <- which.max(Fourier)
+
+points(1:n, mean(precip) +
+  FourierA[largestIndex]*myCos(largestIndex) +
+  FourierB[largestIndex]*mySin(largestIndex), type = "l", col = "red",lwd = 2)
+
+# reconstruct
+recon <- mean(precip)
+for (m in 1:ncoeff) {
+  recon <- recon + FourierA[m]*myCos(m)+FourierB[m]*mySin(m)
+}
+
+plot(1:n, precip,type = "l")
+points(1:n,recon, type = "l", col = "red",lwd = 2)
+# some years are rainier than others due to some spikes
+
+"ncoef=1000 does a very good reconstruction."
+
+ncoeff <- 1000
+
+FourierA <- sapply(1:ncoeff,coeffA)
+FourierB <- sapply(1:ncoeff,coeffB)
+Fourier <- sqrt(FourierA^2+FourierB^2)
+
+# first find the index that contains max value
+largestIndex <- which.max(Fourier)
+
+points(1:n, mean(precip) +
+  FourierA[largestIndex]*myCos(largestIndex) +
+  FourierB[largestIndex]*mySin(largestIndex), type = "l", col = "red",lwd = 2)
+
+# reconstruct
+recon <- mean(precip)
+for (m in 1:ncoeff) {
+  recon <- recon + FourierA[m]*myCos(m)+FourierB[m]*mySin(m)
+}
+
+plot(1:n, precip,type = "l")
+points(1:n,recon, type = "l", col = "red",lwd = 2)
+# well matches the graph, good recon
