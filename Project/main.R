825a0e8ffd36fae8c00911c9f5fae10e330500e3
ihimawan <indri.himawan18@yahoo.com>
2020-05-05 20:44:10 -0400

    conf interal

--- Project/main.R
+++ Project/main.R
@@ -4,35 +4,14 @@ rm(list=ls())
 
 # use fitdistrplus
 # install.packages("fitdistrplus")
-library(fitdistrplus)
+# install.packages("stringr")
+library("fitdistrplus")
+library("stringr")
 
 "The Demographic /r/ForeverAlone Dataset: A survey taken by redditers in /r/ForeverAlone.
 Dataset source: https://www.kaggle.com/kingburrito666/the-demographic-rforeveralone-dataset"
 
-"Required dataset standards.
-[x] A dataframe.
-[x] At least two categorical or logical columns.
-[x] At least two numeric columns.
-[x] At least 20 rows, preferably more, but real-world data may be limited."
 ForeverAlone <- read.csv("lib/foreveralone.csv")
-
-"Required graphical displays (all graphs must be colored and nicely labeled)
-[x] A barplot.
-[x] A histogram.
-[x] A probability density graph overlaid on a histogram.
-[x] A contingency table."
-
-"Required analysis
-[x] A permutation test.
-[x] A p-value or other statistic based on a distribution function.
-[x] Analysis of a contingency table.
-[x] Comparison of analysis by classical methods (chi-square [x], CLT[x]) and simulation methods[x]."
-
-"Required submission uploads
-# [x]  A .csv file with the dataset
-# [x]  A long, well-commented script that loads the dataset, explores it, and does all the analysis.
-# []  A shorter .Rmd with compiled .pdf or .html file that presents highlights in ten minutes.
-# []  A one-page handout that explains the dataset and summarizes the analysis."
 
 # I want to see how many people in this subreddit has not/has attempted suicide depending on by their income
 # In other words, does income level affect likelihood of suicide?
@@ -46,7 +25,93 @@ barplot(orderedCount, main = "Number of suicide attempts by income level",
         las = 1,
         col = c("blue", "red"))
 # to no surprise, it looks like those whose income is the lower end has a higher rate ot suicide attempts..
-# this can also have come to light only because people who use the subreddit are mostly ones with lower income.
+# However, through this barplot can look statistically significant.
+#
+# It's a shame that the columns of salary is an enumeration instead of an actual value.
+# I will create a function that given the text in $x to $y, it will find the average.
+# I will add this as another extra column in the `ForeverAlone`.
+
+
+findAverage <- function(row){
+  incomeRange <- row[5]
+  cleanRange <- str_remove_all(incomeRange, "[$,A-z]")
+  nums <- as.numeric(strsplit(cleanRange, "\\s+")[[1]])
+  if (length(nums) == 1){
+    nums[1]
+  } else {
+    sum(nums)/length(nums)
+  }
+}
+
+ForeverAlone$incomeAverage <- apply(ForeverAlone, 1, findAverage)
+head(ForeverAlone$incomeAverage)
+# [1] 34999.5  5000.5     0.0  5000.5 34999.5 62499.5
+# nice, getting numerical columns now.
+
+# Let's try to use logical regression to see the correlation
+attemptedSuicide <- (as.numeric(ForeverAlone$attempt_suicide=="Yes")); head(attemptedSuicide)
+
+income <- ForeverAlone$incomeAverage
+plot(income, attemptedSuicide)
+b <- cov(income, attemptedSuicide)/var(income) # obtain slope
+# Find the intercept
+a <- mean(attemptedSuicide) - b*mean(income);a
+#We can add this regression line to the plot of the data
+abline(a, b, col = "red") # a negative slope
+
+MLL<- function(alpha, beta) {
+  -sum( log( exp(alpha+beta*income)/(1+exp(alpha+beta*income)) )*attemptedSuicide +
+          log(1/(1+exp(alpha+beta*income)))*(1-attemptedSuicide) )
+}
+
+#R has a function that will maximize this function of alpha and beta
+results<-mle(MLL, start = list(alpha = 0, beta = 0)) #an initial guess is required
+results@coef
+ # alpha          beta
+ # 1.860309e-02 -3.599531e-05
+curve( exp(results@coef[1]+results@coef[2]*x)/ (1+exp(results@coef[1]+results@coef[2]*x)),col = "blue", add=TRUE)
+# The curve shows a good model for the probability of being rich as function of schooling
+
+# test for all the income values to see if it is a good model
+uniqueIncomes <- sort(unique(income))
+#  [1]      0.0   5000.5  14999.5  24999.5  34999.5  44999.5  62499.5  87499.5 112499.5 137499.5
+# [11] 162499.5 187499.0 200000.0
+
+findMean <- function (incomeToEvaluate) {
+  index <- which(income == incomeToEvaluate); head(index)
+  mean(attemptedSuicide[index])   #20%
+}
+
+res <- lapply(uniqueIncomes, findMean)
+cleanRes <- numeric(length(res))
+for (idx in 1: length(res)) {
+  cleanRes[idx] = res[[idx]]
+}
+
+cleanRes
+ # [1] 0.18125000 0.26000000 0.13793103 0.06818182 0.15384615 0.12500000 0.28571429 0.00000000
+ # [9] 0.00000000 0.50000000 0.00000000 0.00000000 0.00000000
+
+# Unfortunately from these numbers it doesnt look very promising for it to be statistically significant.. It fluctuates.
+
+# Permutation test: The mean number of salary for those who  attempted suicide is:
+AttemptedIncomeAvg <- mean(ForeverAlone$incomeAverage[which(ForeverAlone$attempt_suicide == "Yes")]) # 18088.21
+NotAttemptedIncomeAvg <- mean(ForeverAlone$incomeAverage[which(ForeverAlone$attempt_suicide == "No")]) # 20357.94
+observed <- NotAttemptedIncomeAvg - AttemptedIncomeAvg; observed # 2269.724
+
+# now let's use a permutation test
+N <- 10000
+diffs <- numeric(N)
+for (i in 1:N){
+  Attempted <- sample(ForeverAlone$attempt_suicide)   # permuted suicide attempt column
+  AttemptedIncomeAvg <- mean(ForeverAlone$incomeAverage[which(Attempted == 'Yes')])
+  NotAttemptedIncomeAvg <- mean(ForeverAlone$incomeAverage[which(Attempted == 'No')])
+  diffs[i] <- NotAttemptedIncomeAvg - AttemptedIncomeAvg
+}
+
+pvalue <- (sum(diffs >= observed)+1)/(N+1); pvalue # 0.2918708..
+# If we say that statistically significant means they would need to have pvalue of <= 5%, this p-value is quite big (30%)
+# even though it seems like the histogram shows some correlation between income and suicide attempt.
 
 # I expect people who have least friends have a correlation to attempting who attempted suicide
 SuicideFriends <- subset(ForeverAlone, attempt_suicide == "Yes", friends)
@@ -117,8 +182,9 @@ for (i in 1:N){
   diffs[i] <- NotAttemptedFriendNumAvg - AttemptedFriendNumAvg
 }
 
-pvalue <- (sum(diffs >= observed)+1)/(N+1); pvalue # 0.00419958..
-# do reject that having friends and suicide attempt are independent. Meaning that there is some correltion, which is no surprise to me.
+pvalue <- (sum(diffs >= observed)+1)/(N+1); pvalue # 0.00419958
+# do reject that having friends and suicide attempt are independent. Meaning that there is strong correltion, of statistical significance.
+# which is no surprise to me.
 
 # Let's try to make the histogram of the `diff`
 hist(diffs, breaks = "FD", probability = TRUE)
@@ -156,6 +222,54 @@ abline(v = observed, col = "red")
 pvalue <- (sum(diffs >= observed)+1)/(N+1); pvalue # 0.08559144
 # If we're using 0.1 significations level, do reject that having friends and suicide attempt are independent.
 # Meaning that there is some correltion, which is no surprise to me.
+# However if we consider statistical significance is p-value of <=5%, it is not statistically significant after getting rid ot the outliers.
+
+# Let's try to see if using logistic regression can make it seem like it is statistical significant
+attemptedSuicide <- (as.numeric(ForeverAlone.NoFriendOutliers$attempt_suicide=="Yes")); head(attemptedSuicide)
+
+friends <- ForeverAlone.NoFriendOutliers$friends
+plot(friends, attemptedSuicide)
+b <- cov(friends, attemptedSuicide)/var(income) # obtain slope
+# Find the intercept
+a <- mean(attemptedSuicide) - b*mean(friends);a
+#We can add this regression line to the plot of the data
+abline(a, b, col = "red")
+
+MLL<- function(alpha, beta) {
+  -sum( log( exp(alpha+beta*friends)/(1+exp(alpha+beta*friends)) )*attemptedSuicide +
+          log(1/(1+exp(alpha+beta*friends)))*(1-attemptedSuicide) )
+}
+
+#R has a function that will maximize this function of alpha and beta
+results<-mle(MLL, start = list(alpha = 0, beta = 0)) #an initial guess is required
+results@coef
+curve( exp(results@coef[1]+results@coef[2]*x)/ (1+exp(results@coef[1]+results@coef[2]*x)),col = "blue", add=TRUE)
+# shows slight decline as number of friends grow
+
+# test for all the income values to see if it is a good model
+uniqueNumOfFriends <- sort(unique(friends))
+ # [1]  0.0  0.2  0.5  1.0  2.0  3.0  4.0  5.0  6.0  7.0  8.0  9.0 10.0
+
+findMean <- function (numOfFriendsToEvaluate) {
+  index <- which(friends == numOfFriendsToEvaluate); head(index)
+  mean(attemptedSuicide[index])   #20%
+}
+
+res <- lapply(uniqueNumOfFriends, findMean)
+cleanRes <- numeric(length(res))
+for (idx in 1: length(res)) {
+  cleanRes[idx] = res[[idx]]
+}
+
+cleanRes
+#  [1] 0.2293578 0.0000000 1.0000000 0.1666667 0.2142857 0.2045455 0.1851852 0.1777778 0.1923077
+# [10] 0.3000000 0.1000000 0.0000000 0.1176471
+
+# honestly, I do not see this to be the best prediction. I can see that the numbers are generally increasing,
+# but the spikes (like the one on the 10th and last index) makes me think that number of friends is a good
+# prediction model.
+#
+# Even though from the data it looks good and statistically significant, it really isn't (mostly because of removing the outliers)
 
 # Let's try to apply CLT to the number of friends to see if we can get the same normal distribution
 friendsNumber <- ForeverAlone.NoFriendOutliers$friends
@@ -252,3 +366,48 @@ chisq.test(ForeverAlone$depressed, ForeverAlone$attempt_suicide, correct=FALSE)
 # (Same result)
 # With such a small p-value = 2.764e-06, we reject the null hypothesis that depression and suicide attempt are independent.
 # meaning there may be a correlation between those who are depressed and those who have attempted suicide.
+
+# Age of suicide : confidence interval
+SuicideAge <- subset(ForeverAlone, attempt_suicide == "Yes", age)$age
+populationMean <- mean(SuicideAge) # 24.24706
+populationSd <- sd(SuicideAge) # 6.307414
+
+n <- 10
+N <- 10^4
+
+means <- numeric(N)
+stdSamplesT <- numeric(N)
+for (i in 1:N){
+  values <- sample(SuicideAge, n)
+  sampleMean <- mean(values)
+  sampleSd <- sd(values)
+  means[i] <- sampleMean
+  stdSamplesT[i] <- (sampleMean - populationMean)/(sampleSd/sqrt(n))
+}
+
+hist(means, probability = TRUE)
+curve(dnorm(x, populationMean, populationSd/sqrt(10)), add=TRUE, col="red")
+# looks nice
+
+hist(stdSamplesT, probability = TRUE, breaks = "FD")
+curve(dt(x, n-1), add=TRUE, col="red")
+# looks nice.
+
+#Let's see how we do with a t confidence interval
+missedL <- 0; missedU <- 0
+plot(x =c(15,40), y = c(1,100), type = "n", xlab = "", ylab = "") #blank plot
+N <-1000
+counter <- 0
+for (i in 1:N) {
+  x <- sample(SuicideAge, n)
+  L <- mean(x) + qt(0.025, n-1) * sd(x)/sqrt(n) #usually less than the true mean
+  U <- mean(x) + qt(0.975, n-1) * sd(x)/sqrt(n) #usually greater than the true mean
+  if (populationMean <L) missedL <- missedL + 1 # +1 if we were wrong
+  if (populationMean >U) missedU <- missedU + 1 # +1 if we were wrong
+  if (L < populationMean && U > populationMean) counter <- counter + 1 #count +1 if we were correct
+  if(i <= 100) segments(L, i, U, i)
+}
+abline (v = populationMean, col = "red") #vertical line at true mean
+(N-missedL-missedU)/N #what fraction of the time did interval include the true mean?; 0.93
+missedL/N   #the confidence interval rarely lies to the right of the true mean; 0.006
+missedU/N   #the confidence interval often lies to the right of the true mean; 0.064
